b3cbda079b0b703985b53ccbd5ace61e308c2d6d
Profiron <profiron89@gmail.com>
2021-07-14 23:45:19 +0300

    create: added autoplay for slider

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
 const hotelSlider = new Swiper('.hotel-slider', {
-   // Optional parameters
-   loop: true, 
+   // Бесконечный режим прокрутки слайдов
+   loop: true,
+
+   // Скорость прокрутки
+   speed: 1000,
 
    // Navigation arrows
    navigation: {
       nextEl: '.hotel-slider__button--next',
       prevEl: '.hotel-slider__button--prev',
    },
+
+   // Переключение при помощи стрелок
    keyboard: {
       enabled: true,
       onlyInViewport: true,
+   },
+
+   //Автопрокрутка слайдов
+   autoplay: {
+      delay: 3000,
+      // Отключить после ручного переключения
+      disableOnInteraction: false
    }
-});
+})
 
 
 const reviewsSlider = new Swiper('.reviews-slider', {
-   // Optional parameters
+   // Бесконечный режим прокрутки слайдов
    loop: true,
+
+   // Скорость прокрутки
+   speed: 1500,
+
+   //Параллакс на слайдер с отзывами
    parallax: true,
+
    // Navigation arrows
    navigation: {
       nextEl: '.reviews-slider__button--next',
       prevEl: '.reviews-slider__button--prev',
    },
+   // Переключение при помощи стрелок
    keyboard: {
       enabled: true,
       onlyInViewport: true,
+   },
+   //Автопрокрутка слайдов
+   autoplay: {
+      delay: 3000,
+      // Отключить после ручного переключения
+      disableOnInteraction: false
    }
-});
+})
