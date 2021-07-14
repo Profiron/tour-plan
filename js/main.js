fedf89b8cdb9b79b09160b49bf4a3ac6791e2f04
Profiron <profiron89@gmail.com>
2021-07-14 23:24:58 +0300

    create: added parallax for reviews slider

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const hotelSlider = new Swiper('.hotel-slider', {
 const reviewsSlider = new Swiper('.reviews-slider', {
    // Optional parameters
    loop: true,
-
+   parallax: true,
    // Navigation arrows
    navigation: {
       nextEl: '.reviews-slider__button--next',
